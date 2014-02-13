480426852e2df23a19c2f52a4a3524805481d8fd
Santiago Pestarini <santiagonob@gmail.com>
2014-02-13 15:55:29 -0300

    first view
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- views.py
+++ views.py
@@ -5,11 +5,22 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader
 from gea.models import Persona, Expediente, ExpedientePersona
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
-    latest_exp_list = Expediente.objects.order_by('-id')[:5]
-    template = loader.get_template('gea/index.html')
-    context = Context({
-        'latest_exp_list': latest_exp_list,
+    exp_list   = Expediente.objects.order_by('-id')
+    paginator = Paginator(exp_list, 20)
+    page = request.GET.get('page')
+    try:
+        expedientes = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        expedientes = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        expedientes = paginator.page(paginator.num_pages)
+    template = loader.get_template('listado_comun.html')
+    context  = Context({
+        'cl': expedientes,
     })
     return HttpResponse(template.render(context))
